50e67bb1cfc619d45cb8e34ca5c43d5b099dbbea
sergiozc <sergiozapata25@gmail.com>
2024-03-18 19:31:55 +0100

    Integration correlation metrix

--- Simulation/Beamformer/beamformer.m
+++ Simulation/Beamformer/beamformer.m
@@ -24,7 +24,6 @@ maxmax=max(max(abs(y)));
 y=y/maxmax;
 
 % Source
-fs = 11025;
 [source1, fs] = audioread('input/source1.wav');
 source1 = source1(1:92708);
 % Normalization
@@ -43,18 +42,18 @@ DOA = DOA_calc(pos_sensor_ini, pos_source1);
 
 %% Definición de variables
 
-Fs = 11025;         % Frecuencia de muestreo
-d = 0.5;            % Separación entre elementos del array (m)
-Vprop = 340;        % Velocidad del sonido
-Ltrama = 256;       % Tramas de 256 muestras
-Lfft = 512;         % Longitud de la FFT
-N = nsensors;       % Número de sensores
-phi = DOA;         % Ángulo de llegada del target (DOA)
-L_signal = length(y(:,ncent));   %Longitud total de la señal
-win = hanning(Ltrama+1,'periodic'); % Ventana de hanning  
-freq = linspace(0,256,257)*(Fs/Lfft); % Vector de frecuencias
-n=0:1:N-1; % Índice de los elementos del array
-c = 340; % Velocidad de propagación
+Fs = 11025;         % sample rate
+d = 0.5;            % distance between the elemts of the array
+Vprop = 340;        % sound velocity
+Ltrama = 256;       % Frames of 256 samples
+Lfft = 512;         % FFT length
+N = nsensors;       % Number of sensors
+phi = DOA;          % Direction of arrival (DOA)
+L_signal = length(y(:,ncent));   % Signal length
+win = hanning(Ltrama+1,'periodic'); % Hanning window 
+freq = linspace(0,256,257)*(Fs/Lfft); % Frequency array
+n=0:1:N-1; % Index of the elements of the array
+c = 340; % propagation velocity
 
 % Visualization
 dura_cent = length(y(:,ncent)) / Fs;
@@ -65,48 +64,45 @@ title('Central sensor')
 ylabel('Amplitude');
 xlabel('Time (s)');
 
-%% Tipo de onda
-% Se puede elegir entre onda plana o esférica
+%% Wave type definition
+% Flat or spherical can be chosen
 [d_n, tn] = wave_type(c, d, N, phi, 'spherical');
 
 
-%% Cómputo del Beamformer
+%% Beamforming implementation
 
-% CÁLCULO DE PESOS
-% Pesos DAS
+% Weights calculation (delay and sum)
 w = DAS_weights(d_n, tn, freq); fprintf('Beamformer: DAS \n');
-
-% SEÑAL DIVISIBLE ENTRE Ltrama 
+ 
+% Divisible signal into Ltrama
 [m,~] = size(y);
 resto = mod(m,Ltrama);
 y = y(1:m-resto,:);
-% Se obtiene el número de muestras que tendrá la señal sobre la que se
-% aplicará el beamforming
 [m,~] = size(y); 
 
 Ntramas = 2*(m/Ltrama)-1;
 
-%% PROCESADO POR TRAMAS (análisis-síntesis)
+%% Frames processing (analysis-synthesis)
 
-xc_out = zeros(L_signal,N); % Matriz del resultado final
-XOUT = zeros(Lfft/2+1, 1); % Señal depúes de aplicar los pesos
+xc_out = zeros(L_signal,N); % Final matrix
+XOUT = zeros(Lfft/2+1, 1); % Signal after applying weights
 iter = 1;
-for ntram = 1:Ntramas  % Se computa cada trama
+for ntram = 1:Ntramas  % each frame
 
-    for c = 1:N        % Se computa cada sensor
+    for c = 1:N        % each sensor
         
-        xn = y(iter:iter + Ltrama ,c); %Tomamos la porción de señal del canal correspondiente
-        Xn = fft(win.*xn, Lfft);        %Realizamos la transformada de Fourier de la ventana (512 muestras)
-        Xn = Xn(1:Lfft/2+1);          %Tomamos las componentes de frecuencia de 0 a Fs/2 (Fs/2 = 8 kHz)s     
-        Xn = Xn .* conj(w(:,c));        %Multiplicamos por los pesos correspondientes
+        xn = y(iter:iter + Ltrama ,c); % a piece of the signal
+        Xn = fft(win.*xn, Lfft);        % We perform the Fourier transform of the window (512 samples)
+        Xn = Xn(1:Lfft/2+1);          % We take the frequency components from 0 to Fs/2    
+        Xn = Xn .* conj(w(:,c));        % Multiplying by the weights
         
 
-        %Realizamos la simetrización para practicar la transformada inversa
+        % Forcing symmetry
         simet = conj(Xn);
         XOUT = cat(1, Xn, simet(end:-1:2));
         xout = real(ifft(XOUT));
         
-        %Concatenación de tramas mediante ''overlap add''
+        % Frame concatenation''overlap add''
         xc_out(iter:iter + Lfft, c) = xc_out(iter:iter + Lfft, c) + xout;
 
     end
@@ -114,15 +110,15 @@ for ntram = 1:Ntramas  % Se computa cada trama
     iter = iter + (Ltrama/2-1);
 end
 
-%Unimos todos los canales
+% Joining all channels
 xc_out_sum = sum(xc_out, 2);
-% Eliminamos la cola residual de la ultima trama
+% Eliminating residual tail of the last frame
 xc_out_sum = xc_out_sum(1:end-Lfft/2);
-% Normalizamos la señal y la escuchamos
+% Normalization and listening
 xout_norm = xc_out_sum/max(abs(xc_out_sum));
 soundsc(real(xout_norm),Fs);
 
-% Guardamos señal resultante normalizada
+% Saving the signal
 fout=strcat('./results/DAS_result','.wav');
 audiowrite(fout, xout_norm, Fs)
 
@@ -137,23 +133,39 @@ title('Time representation after beamforming')
 %Se puede comprobar como el ruido se ha minimizado
 
 
-%% SALE LA POTENCIA DEL RUIDO NEGATIVA!!! REVISAR
-%% Cálculo SNR
+%% Correlation metrix
 
-% Calcular la potencia de la señal deseada
+% BEFORE BEAMFORMING
+% Correlation matrix
+corr_matrix2 = corrcoef(source1(1:length(y(:,1))), y(:,1));
+% Correlation between both signals
+corr_coef2 = corr_matrix2(1, 2);
+
+% AFTER BEAMFORMING
+% Correlation matrix
+corr_matrix1 = corrcoef(source1(1:length(xout_norm)), xout_norm);
+% Correlation between both signals
+corr_coef1 = corr_matrix1(1, 2);
+
+fprintf('Correlation coefficient before BF = %f\n', corr_coef2);
+fprintf('Correlation coefficient after BF = %f\n', corr_coef1);
+
+%% SNR metrix (not useful...)
+
+% Source power
 power_source = var(source1);
-% Calcular la potencia de la señal de entrada
+% Input power (signal mixed)
 power_input = var(y(:, 1));
-% Calcular la potencia de la señal de salida del beamformer
+% Output (after beamforming) power
 power_output = var(xout_norm);
-% Calcular la diferencia entre la potencia de la señal deseada y la de
-% entarda
-power_noise = max(power_input - power_output, 0);
+% The diference between the input power and the source power is the noise
+% power
+power_noise = max(power_input - power_source, 0);
 
-% SNR ANTES DEL BEAMFORMING
+% SNR before beamforming
 SNR_orig = SNR_calc(power_input, power_noise);
 fprintf('SNR(before)  = %f dB\n', SNR_orig);
 
-% SNR DESPUÉS DEL BEAMFORMING
+% SNR after beamforming
 SNR_BF = SNR_calc(power_output, power_noise);
 fprintf('SNR(after)  = %f dB\n', SNR_BF);
