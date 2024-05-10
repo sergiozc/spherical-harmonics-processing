c99ac277863f0b5e8f518ebae9179b1bc51f7cfe
sergiozc <sergiozapata25@gmail.com>
2024-05-10 19:20:09 +0200

    Implementing bridge between collected data and algorithm

--- Simulation/PSD-algorithm/main.py
+++ Simulation/PSD-algorithm/main.py
@@ -13,51 +13,46 @@ import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 # %%
-# PARAMETER DEFINITION (valores de prueba)
+# PARAMETER DEFINITION
 
-Q = 32  # Number of microphones
 r = 0.042 # Assuming same radius
-freq_array = loadmat('data/freq.mat')['freq_array'] 
-freq_array[0] = 0.1 # To avoid dividing by zero
 c = 343 # Propagation speed
-k_array = SHutils.getK(freq_array, c) # Wavenumber array
-Nfreq = len(k_array) # Number of frequencies
 
-freq = 882.861 # Frequency (unit test)
-k = SHutils.getK(freq, c)
+pos_mic = loadmat('data/pos_mic.mat')['pos_mic'] # Microphones positions (x,y,z)
+Q = pos_mic.shape[0]  # Number of microphones (selecting rows' length) 
+el = np.zeros(Q) # Mic elevation
+az = np.zeros(Q) # Mic azimut
+for i, (x, y, z) in enumerate(pos_mic): # Switch to el and az (from cartesian)
+    el[i], az[i] = utils.cartesian2ElAz(x, y, z)
 
-order = 4 # Spherical harmonic order (it defines the complexity of the spherical function).
-# Defined in the "experimental setup" of the paper. Highest order to calculate.
-
-Nmin = 2 # Minimun sound order
-
-el = np.linspace(0, np.pi, Q) # Microphones elevation [0, pi] (rad). 
-az = np.linspace(0, 2*np.pi, Q) # Microphones azimuth [0, 2*pi] (rad)
-r = np.full((Q, 1), 0.042) # Radial distances (to the origin of the array).
-# If radius distances are the same. Must be a number.
-
-# Using cartesian coordenates
-# el, az = utils.cartesian2ElAz(1, 4, -1)
-
-L = 5 # Number of sources
-timeFrames = 500 # Number of time frames
-V = int(np.floor(np.sqrt((order + 1)**2 - L - 1) - 1)) # Order of the power of a reverberation sound field.
-# See Eq (59) from the paper. # The higher the order the greater the reverberation complexity.
-
-
+pos_sources = loadmat('data/pos_sources.mat')['pos_sources'] # Sources positions (x,y,z)
+L = pos_sources.shape[0]  # Number of sources (selecting rows' length)
 # Sources' configuration
 el_s = np.linspace(0, np.pi, L)  # Sources elevation [0, pi] (rad). 
 az_s = np.linspace(0, 2 * np.pi, L)  # # Microphones azimuth [0, pi] (rad). 
-r_s = np.linspace(0, 2, L)  # Radius [0,2]
 
-# Sources' amplitudes
-amp_s = (np.linspace(0, 10, L)[:, np.newaxis] * np.ones((L, timeFrames))).T  # Amplitudes [0 ,10]
+el_s = np.zeros(L) # Sources elevation
+az_s = np.zeros(L) # Sources azimut
+for i, (x, y, z) in enumerate(pos_sources):# Switch to el and az (from cartesian)
+    el_s[i], az_s[i] = utils.cartesian2ElAz(x, y, z)
 
-# Sound pressure (este dato lo tenemos que sacar del experimento)
-P = np.ones((32, 500)) + 1j * np.zeros((32, 500))
 
-P_vector = np.ones((Q, len(freq_array), timeFrames)) + 1j * np.zeros((Q, len(freq_array), timeFrames))
+freq_array = loadmat('data/freq.mat')['freq_array'] # Frequency array
+freq_array[0] = 0.1 # To avoid dividing by zero
+k_array = SHutils.getK(freq_array, c) # Wavenumber array
+Nfreq = len(k_array) # Number of frequencies
 
+
+P = loadmat('data/sound_pressure.mat')['P'] # Sound pressure
+P = np.transpose(P, (0, 2, 1)) # Adjust to a (freq x Q x time) tensor
+timeFrames = P.shape[2] # Number of time frames
+
+
+order = 4 # Spherical harmonic order (it defines the complexity of the spherical function).
+# Defined in the "experimental setup" of the paper. Highest order to calculate.
+Nmin = 2 # Minimun sound order
+V = int(np.floor(np.sqrt((order + 1)**2 - L - 1) - 1)) # Order of the power of a reverberation sound field.
+# See Eq (59) from the paper. # The higher the order the greater the reverberation complexity.
 
 #%%
 # SH calculations (just to test if necessary)
@@ -99,10 +94,13 @@ applyRegularization = False
 alpha = np.zeros((Nfreq, (order+1)**2, timeFrames), dtype=complex)
 # Frequency loop
 for k_index, k in enumerate(k_array):
-    alpha[k_index, :, :] = SHutils.alphaOmni(P, order, k, r, el, az, isRigid, mode, compensateBesselZero, applyRegularization, weights)
+    alpha[k_index, :, :] = SHutils.alphaOmni(P[k_index, :, :], order, k, r, el, az, isRigid, mode, compensateBesselZero, applyRegularization, weights)
     
 #%% 
 # UPSILON, PSI, OMEGA calculation (3rd step of the algorithm)
+
+#freq = 882.861 # Frequency (unit test)
+#k = SHutils.getK(freq, c)
 
 #ups = PSDestimation.upsilon_term(0, 0, 0, 0, el_s, az_s)
 #psi = PSDestimation.psi_term(0, 0, 0, 0, 2, 1)
